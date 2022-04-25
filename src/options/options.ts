6d1fc69263498d0b3b6de151380cec32b34402ee
Bhanu Pratap Singh <42886724+hiiambhanu@users.noreply.github.com>
2022-04-25 08:23:21 +0530

    Add automatic function discovery (#11)
    * add: automatic function discovery
    
    * fix: typo
    
    * Fix: lint
    
    * fix:test
    
    * fix: functions disovery test
    
    * minor changes

--- src/options/options.ts
+++ src/options/options.ts
@@ -9,10 +9,12 @@ export const options: OptionDefinition[] = [
     { name: "sep", type: String, defaultValue: "," },
     { name: "bundlerConfig", type: String, defaultValue: "" },
     { name: "concurrency", type: Number },
+    { name: "discover", type: Boolean, defaultValue: true },
+    { name: "indexFilePath", type: String, defaultValue: undefined },
 ];
 
 const args = cmdArgs(options, { partial: true });
-const { dir, write, verbose, prefix, sep: separator, bundlerConfig, concurrency } = args;
+const { dir, write, verbose, prefix, sep: separator, bundlerConfig, concurrency, discover, indexFilePath } = args;
 if (!dir) {
     console.error("Error: dir argument not supplied");
     process.exit(1);
@@ -21,4 +23,15 @@ if (!dir) {
 const specFilePath: string = path.join(dir, ".differspec.json");
 const bundlerConfigFilePath: string = bundlerConfig ? path.join(dir, bundlerConfig) : "";
 
-export { specFilePath, dir, write, verbose, prefix, separator, bundlerConfigFilePath, concurrency };
+export {
+    specFilePath,
+    dir,
+    write,
+    verbose,
+    prefix,
+    separator,
+    bundlerConfigFilePath,
+    concurrency,
+    discover,
+    indexFilePath,
+};
