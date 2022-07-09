dd018196386ca125e3b25faebc82bf365e187b13
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2022-07-09 10:36:06 +0530

    fix: Add a no-discover flag to disable automatic discovery

--- src/options/options.ts
+++ src/options/options.ts
@@ -10,11 +10,14 @@ export const options: OptionDefinition[] = [
     { name: "bundlerConfig", type: String, defaultValue: "" },
     { name: "concurrency", type: Number },
     { name: "discover", type: Boolean, defaultValue: true },
+    { name: "no-discover", type: Boolean },
     { name: "indexFilePath", type: String, defaultValue: undefined },
 ];
 
 const args = cmdArgs(options, { partial: true });
-const { dir, write, verbose, prefix, sep: separator, bundlerConfig, concurrency, discover, indexFilePath } = args;
+const { dir, write, verbose, prefix, sep: separator, bundlerConfig, concurrency, indexFilePath } = args;
+const discover = args.discover && !args["no-discover"];
+
 if (!dir) {
     console.error("Error: dir argument not supplied");
     process.exit(1);
