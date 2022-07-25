18490beb4478f614b4e1f883683826f4863cdc4a
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2022-07-25 17:35:54 +0530

    Add Type Checker based automatic function discovery (#14)
    * feat: Add Type Checker based automatic function discovery
    
    * fix: Remove leading / from test paths

--- src/discoverer/discoverer.spec.ts
+++ src/discoverer/discoverer.spec.ts
@@ -5,9 +5,9 @@ describe("Function Discovery", function () {
     this.timeout(0);
     it("Should return the functions paths correctly", () => {
         const expectedResult = {
-            firstFunction: "/test-e2e/firstFunctions/function.ts",
-            secondFunctionHttpTrigger: "/test-e2e/secondFunction/http.trigger.ts",
-            secondFunctionPubSub: "/test-e2e/secondFunction/pubsub.trigger.ts",
+            firstFunction: "test-e2e/firstFunctions/function.ts",
+            secondFunctionHttpTrigger: "test-e2e/secondFunction/http.trigger.ts",
+            secondFunctionPubSub: "test-e2e/secondFunction/pubsub.trigger.ts",
         };
         const indexFilePath = "test-e2e/index.ts";
         const result = getFirebaseFunctionsAndPaths(indexFilePath);
