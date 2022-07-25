39daa02620fd6dd612cdf34e4059c03c91a6c62a
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2022-07-25 17:10:03 +0530

    feat: Add Type Checker based automatic function discovery

--- src/discoverer/discoverer.ts
+++ src/discoverer/discoverer.ts
@@ -1,9 +1,7 @@
-import { ExportDeclaration, ImportDeclaration, Project } from "ts-morph";
-
-const getPath = (declaration: ImportDeclaration | ExportDeclaration): string | undefined => {
-    const sourceFile = declaration.getModuleSpecifierSourceFile()?.getFilePath()?.toString();
-    return sourceFile;
-};
+import { relative } from "path";
+import { cwd } from "process";
+import { Project } from "ts-morph";
+import logger from "../logger";
 
 /**
  * Function to discover and get the paths of all functions
@@ -12,71 +10,31 @@ const getPath = (declaration: ImportDeclaration | ExportDeclaration): string | u
  * @returns {Record<string, string>} A Record containing list of all functions as keys and their paths as values.
  */
 const getExports = (indexFilePath = "src/index.ts"): Record<string, string> => {
-    if (indexFilePath.startsWith("undefined")) {
-        return {};
-    }
-    const sourceFile = project.getSourceFileOrThrow(indexFilePath);
+    const exports: Record<string, string> = {};
 
-    // Build imports map, as imports can have paths
-    const imports = sourceFile.getImportDeclarations();
-    const importMap: Record<string, string> = {};
-    for (const imp of imports) {
-        const path = getPath(imp);
+    const typeChecker = project.getTypeChecker();
+    const indexFile = project.getSourceFileOrThrow(indexFilePath);
 
-        [...imp.getNamedImports(), imp.getDefaultImport()].map((name) => {
-            if (name && path) importMap[name.getText()] = path;
-        });
-    }
+    const exportSymbols = typeChecker.getExportsOfModule(indexFile.getSymbolOrThrow());
+    for (const exportSymbol of exportSymbols) {
+        const exportName = exportSymbol.getName();
+        const aliasedSymbol = exportSymbol.getAliasedSymbol();
 
-    const exports = sourceFile.getExportDeclarations();
-    let exportMap: Record<string, string> = {};
+        const [declaration] = aliasedSymbol ? aliasedSymbol.getDeclarations() : exportSymbol.getDeclarations();
 
-    for (const exp of exports) {
-        const ppath = getPath(exp);
-        const exportedFunctions = exp.getNamedExports();
-
-        for (const func of exportedFunctions) {
-            let path = ppath;
-            if (exportMap[func.getText()]) continue;
-            if (!path) path = importMap[func.getText()];
-
-            // Assumption: Files that import modules from another file and export it have the word "index" in it.
-            // Need to recursively call the getExports function on this index.ts file to get the actual path of the
-            // functions exported from this index file.
-            if (path?.includes("index")) {
-                let currentDir = exp.getModuleSpecifierSourceFile()?.getDirectoryPath();
-
-                // if currentDir is undefined, try to get currentDir from the imports. This can happen if a file has
-                // separate import-export lines for the module
-
-                if (!currentDir) {
-                    const imp = imports.filter((impp) =>
-                        impp
-                            .getNamedImports()
-                            .map((x) => x.getText())
-                            .includes(func.getText()),
-                    );
-                    currentDir = imp[0].getModuleSpecifierSourceFile()?.getDirectoryPath();
-                }
-
-                const deeperExports = getExports(`${currentDir}/index.ts`);
-
-                // if the deeperExports object is empty, it possibly means that
-                // the function was defined in the most recent "index*.ts" file explored
-                // or there was a failure. In both cases, assume the path of the function to be the
-                // most recent "index*.ts" file explored
-                if (Object.keys(deeperExports).length === 0) {
-                    exportMap[func.getText()] = path;
-                } else {
-                    exportMap = { ...exportMap, ...deeperExports };
-                }
-            } else {
-                if (path) exportMap[func.getText()] = path;
-            }
+        if (!declaration) {
+            logger.error(`Failed to find a declaration for ${exportSymbol.getName()}`);
+            continue;
         }
+
+        const filePath = declaration.getSourceFile().getFilePath();
+
+        exports[exportName] = relative(cwd(), filePath);
     }
-    return exportMap;
+
+    return exports;
 };
+
 const project = new Project();
 project.addSourceFilesAtPaths("**/*.ts");
 
