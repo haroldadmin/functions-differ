c5ac918be07526d04300079902e6a1fb4533bae9
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2022-07-25 17:22:57 +0530

    fix: Expect relative paths in discoverer test

--- src/discoverer/discoverer.spec.ts
+++ src/discoverer/discoverer.spec.ts
@@ -4,11 +4,10 @@ import { getFirebaseFunctionsAndPaths } from "./discoverer";
 describe("Function Discovery", function () {
     this.timeout(0);
     it("Should return the functions paths correctly", () => {
-        const dir = process.cwd().replace(/\\/g, "/");
         const expectedResult = {
-            firstFunction: `${dir}/test-e2e/firstFunctions/function.ts`,
-            secondFunctionHttpTrigger: `${dir}/test-e2e/secondFunction/http.trigger.ts`,
-            secondFunctionPubSub: `${dir}/test-e2e/secondFunction/pubsub.trigger.ts`,
+            firstFunction: "/test-e2e/firstFunctions/function.ts",
+            secondFunctionHttpTrigger: "/test-e2e/secondFunction/http.trigger.ts",
+            secondFunctionPubSub: "/test-e2e/secondFunction/pubsub.trigger.ts",
         };
         const indexFilePath = "test-e2e/index.ts";
         const result = getFirebaseFunctionsAndPaths(indexFilePath);
