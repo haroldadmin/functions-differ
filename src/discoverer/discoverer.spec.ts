afcc8d27acad6c0965ea0878304b1a89db450d09
Cedric Deege <cedric.deege@gmx.de>
2023-10-02 12:07:48 +0200

    feat: Automatically detect nested functions organised in folders.

--- src/discoverer/discoverer.spec.ts
+++ src/discoverer/discoverer.spec.ts
@@ -8,6 +8,7 @@ describe("Function Discovery", function () {
             firstFunction: "test-e2e/firstFunctions/function.ts",
             secondFunctionHttpTrigger: "test-e2e/secondFunction/http.trigger.ts",
             secondFunctionPubSub: "test-e2e/secondFunction/pubsub.trigger.ts",
+            "nestedFunctions.deeplyNestedFunction": "test-e2e/nestedFunctions/deeplyNested.trigger.ts",
         };
         const indexFilePath = "test-e2e/index.ts";
         const result = getFirebaseFunctionsAndPaths(indexFilePath);
